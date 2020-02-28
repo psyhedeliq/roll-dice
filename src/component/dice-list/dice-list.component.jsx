05c63c80270a3aae51ffb90e56bc4c6c64893444
psyhedeliq <filipcornelcristian@gmail.com>
2020-02-28 07:03:04 +0000

    create and style dice component

--- src/component/dice-list/dice-list.component.jsx
+++ src/component/dice-list/dice-list.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { getDice } from '../../util';
 import { RollButton } from '../roll-button';
+import { Dice } from '../dice';
 
 const DiceList = () => {
   const [diceList, rollDice] = useState(getDice());
@@ -9,7 +10,7 @@ const DiceList = () => {
     rollDice(getDice());
   };
   return [
-    ...diceList.map((dice, key) => <div key={key}>{dice}</div>),
+    ...diceList.map((dice, key) => <Dice key={key}>{dice}</Dice>),
     <RollButton key='button' handleOnClick={handleOnClick} />
   ];
 };
